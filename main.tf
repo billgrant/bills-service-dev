94649b4917b754da0e461bbd860ad1e173a0fa0d
Bill Grant <39649812+billgrant@users.noreply.github.com>
2025-08-06 10:03:13 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+# Boilerplate Terraform configuration to test the random_pet resource
+resource "random_pet" "pet" {}
+
+# module "s3" {
+#   source  = "app.terraform.io/billgrant/s3/aws"
+#   version = "~> 1.1.0"
+# }
+
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "pet1_name" {
+  value = random_pet.pet.id
+}
+
+# output "bucket_name" {
+#   value = module.s3.bucket_name
+# }
+
+# output "bucket_arn" {
+#   value = module.s3.bucket_arn
+# }
